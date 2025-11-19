7f5949c2dd91f8140569a017d81846e7f7d454f0
pyetro-cod <pyetro.f@aluno.uepb.edu.br>
2025-11-19 01:16:24 -0300

    Ex005 Calculando Resto da Divisão

---
+++ fundamentos/operadores/ex05caculandoResto.java
@@ -0,0 +1,11 @@
+package fundamentos.operadores;
+
+public class ex05caculandoResto {
+    public static void main(String[] args) {
+        double dividendo = 20;
+        double divisor = 40;
+
+        double divisao = dividendo % divisor;
+        System.out.println("O resto da divisão de " + dividendo + " por " + divisor + " é: " + divisao);
+    }   
+}
